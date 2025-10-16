ff61bd554a9c25f9f613491ade4ef02a3e2b0c57
Scout Knight-Pheng <sknightp@calpoly.edu>
2025-10-15 20:29:10 -0700

    feat: create blog.js from .ts file

---
+++ src/blog.js
@@ -0,0 +1,26 @@
+var blogs = [
+    {
+        title: 'First Blog Post',
+        date: '2025-10-15',
+        description: 'This is the description of the first blog post.',
+        image: 'me.jpg',
+        imageAlt: 'Image 1 description',
+        slug: 'first-blog-post',
+    },
+];
+var blogContainer = document.getElementById('blog-container');
+blogs.forEach(function (blog) {
+    var blogElement = document.createElement('div');
+    var blogTitle = document.createElement('h2');
+    var blogImage = document.createElement('img');
+    var blogParagraph = document.createElement('p');
+    blogImage.src = blog.image;
+    blogImage.alt = blog.imageAlt;
+    blogElement.appendChild(blogTitle);
+    blogElement.appendChild(blogImage);
+    blogElement.appendChild(blogParagraph);
+    if (blogContainer) {
+        blogContainer.appendChild(blogElement);
+    }
+});
+console.log('Blog script loaded');
